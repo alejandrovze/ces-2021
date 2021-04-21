07dcefbc3fb98c8dfce26e305e72c7c0cca7138d
alejandrovze <alejandrovze@gmail.com>
2021-04-21 17:40:08 +0200

    Note d'intention

---
+++ Assets/Scripts-AVZE/ChangeLight.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ChangeLight : MonoBehaviour
+{
+    Light lt;
+
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+
+    // Darken the light completely over a period of 2 seconds.
+    void Update()
+    {
+        lt.color -= (Color.white / 2.0f) * Time.deltaTime;
+    }
+}
